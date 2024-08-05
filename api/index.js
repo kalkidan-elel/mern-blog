122d0784251b0721f4a44ea1d5ba5d001674fc30
Kal@git <kal4diffacc@gmail.com>
2024-08-05 11:53:40 -0700

    Connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();   //dotenv dependencies used to use the .env in the app
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("MongoDB is Connected");
+    }).catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
