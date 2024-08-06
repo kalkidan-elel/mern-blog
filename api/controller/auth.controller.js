dc7c4d5d0bbfcbfc2b5fe2b7b208132edcd5b542
Kal@git <kal4diffacc@gmail.com>
2024-08-06 06:33:17 -0700

    Create sign in api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
-import bctyptjs from 'bcryptjs';
+import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -9,7 +10,7 @@ export const signup = async (req, res, next) => {
         next(errorHandler(400, "All fields are required"));
     }
 
-    const hashedpassword = bctyptjs.hashSync(password, 10);
+    const hashedpassword = bcryptjs.hashSync(password, 10);
     const newuser = new User({
         username,
         email,
@@ -23,5 +24,35 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error)
     }
+};
 
-}
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required'));
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(400, 'Invalid password'));
+        }
+        const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET);
+
+        const { password: pass, ...rest } = validUser._doc; //This hides the password
+
+        res
+            .status(200)
+            .cookie('access_token', token, {
+                httpOnly: true,
+            })
+            .json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
