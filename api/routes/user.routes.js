ba1419c9c10dd672580f10bfcd44b1b0c248763e
Kal@git <kal4diffacc@gmail.com>
2024-08-09 18:25:23 -0700

    Complete delete user account functionality

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { test, updateUser } from '../controller/user.controller.js';
+import { deleteUser, test, updateUser } from '../controller/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js'
 
 const routes = express.Router();
 
 routes.get('/test', test)
 routes.put('/update/:userId', verifyToken, updateUser);
+routes.delete('/delete/:userId', verifyToken, deleteUser)
 
 export default routes
