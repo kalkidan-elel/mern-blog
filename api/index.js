7e9a27dec3c27402c640b737f1f7c4bf4c23e09e
Kal@git <kal4diffacc@gmail.com>
2024-08-13 08:27:30 -0700

    Add comment section to the post page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import postRoutes from './routes/post.routes.js';
+import commentRoutes from './routes/comment.routes.js';
 import cookieParser from 'cookie-parser';
 
 //There is no userRoutes in ./routes/user.routes.js but it works well.
@@ -29,6 +30,7 @@ app.listen(3000, () => {
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
 //Add a middleware and a function to handle error
 app.use((err, req, res, next) => {
