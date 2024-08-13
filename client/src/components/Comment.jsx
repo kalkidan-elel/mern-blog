af471e2e31d6c12663b1f96d4edeaeedd930d93f
Kal@git <kal4diffacc@gmail.com>
2024-08-13 09:57:10 -0700

    Add like functionality to the comment component

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,7 +4,8 @@ import { FaThumbsUp } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import { Button, Textarea } from 'flowbite-react';
 
-export default function Comment({ comment }) {
+export default function Comment({ comment, onLike }) {
+    const { currentUser } = useSelector((state) => state.user);
     const [user, setUser] = useState({})
     useEffect(() => {
         const getUser = async () => {
@@ -33,7 +34,20 @@ export default function Comment({ comment }) {
                     </span>
                     <span className='text-gray-500 text-xs'>{moment(comment.createdAt).fromNow()}</span>
                 </div>
-
+                <p className='text-gray-500 pb-2'>{comment.content}</p>
+                <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
+                    <button
+                        type='button'
+                        onClick={() => onLike(comment._id)}
+                        className={`text-gray-400 hover:text-blue-500 ${currentUser &&
+                            comment.likes.includes(currentUser._id) && '!text-blue-500'}`}>
+                        <FaThumbsUp className='text-sm' />
+                    </button>
+                    <p className='text-gray-400'>
+                        {comment.numberOfLikes > 0 && comment.numberOfLikes + ' ' +
+                            (comment.numberOfLikes === 1 ? 'like' : 'likes')}
+                    </p>
+                </div>
             </div>
         </div>
     )
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -48,7 +48,25 @@ export default function CommentSection({ postId }) {
         getComments();
     }, [postId]);
 
-    console.log(comments)
+    const handleLike = async (commentId) => {
+        try {
+            if (!currentUser) {
+                navigate('/sign-in');
+                return;
+            }
+            const res = await fetch(`/api/comment/likeComment/${commentId}`, { method: 'PUT', });
+            if (res.ok) {
+                const data = await res.json();
+                setComments(
+                    comments.map((comment) => comment._id === commentId ? {
+                        ...comment, likes: data.likes, numberOfLikes: data.likes.length,
+                    } : comment)
+                );
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
 
     return (
         <div className='max-w-2xl mx-auto w-full p-3'>
@@ -100,7 +118,7 @@ export default function CommentSection({ postId }) {
                         <Comment
                             key={comment._id}
                             comment={comment}
-                        // onLike={handleLike}
+                            onLike={handleLike}
                         // onEdit={handleEdit}
                         // onDelete={(commentId) => {
                         //     setShowModal(true);
